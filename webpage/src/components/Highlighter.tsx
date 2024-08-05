b46c4f120f8e8c8a234f714cde71f3d34a156979
thewilley <89783791+TheWilley@users.noreply.github.com>
2024-08-05 21:38:01 +0200

    style: fixed issue where rounded corners would not appear at the bottom of long textures due to overflow

--- webpage/src/components/Highlighter.tsx
+++ webpage/src/components/Highlighter.tsx
@@ -12,12 +12,12 @@ function Highlighter({ texture, highlightedArea }: Props) {
   useHighlight(texture, highlightedArea, canvasRef);
 
   return (
-    <div className='overflow-auto max-h-[calc(100vh-80px)] top-[70px] sticky z-10 hidden md:block'>
+    <div className='overflow-auto max-h-[calc(100vh-80px)] top-[70px] sticky z-10 hidden md:block rounded-md'>
       <canvas
         ref={canvasRef}
         width={texture.size.x}
         height={texture.size.y}
-        className='w-full bg-base-200 p-5 rounded-md'
+        className='w-full bg-base-200 p-5'
       />
     </div>
   );
