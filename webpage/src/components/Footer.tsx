f03511909a8f8e3aeae2636e12afe5fa8fb7fa26
thewilley <89783791+TheWilley@users.noreply.github.com>
2024-08-06 19:46:41 +0200

    style: updated footer styling

--- webpage/src/components/Footer.tsx
+++ webpage/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className='mt-5'>
+    <footer className='mt-5 bg-base-200 w-full p-3 rounded-full opacity-70'>
       <p> Made by TheWilley </p>
       <p>
         {' '}
