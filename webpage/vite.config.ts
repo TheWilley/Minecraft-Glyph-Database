29146878d144f35e5b6b6f68d2cdeac87c3e30df
thewilley <89783791+TheWilley@users.noreply.github.com>
2025-07-12 21:14:18 +0200

    chore: updated dependencies
    vite, tailwindcss, daisyui

--- webpage/vite.config.ts
+++ webpage/vite.config.ts
@@ -1,7 +1,8 @@
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default {
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   base: '/Minecraft-Glyph-Database/'
 };
