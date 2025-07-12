925d05bf88a564322ed63c3a359e6be5af0c38a9
thewilley <89783791+TheWilley@users.noreply.github.com>
2025-07-12 22:10:48 +0200

    style: made footer less rounded and header more

--- webpage/src/components/Footer.tsx
+++ webpage/src/components/Footer.tsx
@@ -5,7 +5,7 @@
  */
 function Footer() {
   return (
-    <footer className='mt-5 bg-base-200 w-full p-3 rounded-full opacity-70'>
+    <footer className='mt-5 bg-base-200 w-full p-3 rounded-md opacity-70'>
       <p> Made by TheWilley </p>
       <p>
         This project is in no shape or form affiliated with or endorsed by Mojang and
